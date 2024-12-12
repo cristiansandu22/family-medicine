16ce917b5d8bbf42e275650477acbdfb4c3a87fe
cristiansandu22 <cristian.sandu@welthee.com>
2024-12-12 17:18:22 +0200

    Test routes + change favicon
    - Testing routes
    - Change favicon

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function () {
+   return response()->json(['message' => 'Hello from Laravel API, boss!']);
+});
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,8 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+Route::get('/test-for-web', function () {
+    return response()->json(['message' => 'This is a test.']);
+});
+
 require __DIR__.'/auth.php';
